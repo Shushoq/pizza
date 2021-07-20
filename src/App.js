f7f9f103cd60b0efcb3adf1dfd992ec07088392c
Evgeniy Shushkov <59647498+Shushoq@users.noreply.github.com>
2021-07-20 22:51:18 +0300

    feat: remove unnecessery

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import axios from 'axios'
-import React, { useEffect } from 'react'
+import React from 'react'
 import { Route } from 'react-router-dom'
-import { useDispatch } from 'react-redux'
 
 import Header from './components/Header'
 import Cart from './pages/Cart'
 import Home from './pages/Home'
 
-import { setPizzas } from './redux/actions/pizzas'
 
 const App = () => {
-  const dispatch = useDispatch()
-
-  useEffect(() => {
-    axios.get('./db.json').then(({ data }) => {
-      dispatch(setPizzas(data.pizzas))
-    })
-  }, [])
-
   return (
     <div className="wrapper">
       <Header />
@@ -30,42 +19,3 @@ const App = () => {
 }
 
 export default App
-
-// class App extends React.Component {
-//   componentDidMount() {
-//     axios.get('./db.json').then(({ data }) => {
-//       this.props.setPizzas(data.pizzas)
-//     })
-//   }
-
-//   render() {
-//     return (
-//       <div className="wrapper">
-//         <Header />
-//         <div className="content">
-//           <Route
-//             path="/"
-//             exact
-//             render={() => <Home items={this.props.items} />}
-//           />
-//           <Route path="/cart" exact component={Cart} />
-//         </div>
-//       </div>
-//     )
-//   }
-// }
-
-// const mapStateToProps = (state) => {
-//   return {
-//     items: state.pizzas.items,
-//     filters: state.filters,
-//   }
-// }
-
-// const mapDispatchToProps = (dispatch) => {
-//   return {
-//     setPizzas: (items) => dispatch(setPizzas(items)),
-//   }
-// }
-
-// export default connect(mapStateToProps, mapDispatchToProps)(App)
